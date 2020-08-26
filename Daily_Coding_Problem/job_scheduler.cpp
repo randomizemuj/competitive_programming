a5797c15c010ec4d558e9639df66eff565eb0df9
randomizemuj <45651729+randomizemuj@users.noreply.github.com>
2020-08-26 23:37:24 +0530

    Update job_scheduler.cpp

--- Daily_Coding_Problem/job_scheduler.cpp
+++ Daily_Coding_Problem/job_scheduler.cpp
@@ -1,33 +1,46 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
 
-import java.util.Timer;
-import java.util.TimerTask;
-public class Main
-{
-    public static void main(String args[]){
-	solution(new Command() {
-            @Override
-            public void execute() {
-                {
-                    System.out.println("Hello World");
-                }
-            }
-        }, 1000);
+auto printElapsedTime(std::chrono::time_point<std::chrono::high_resolution_clock> startTime)
+    -> void {
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto tElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    std::cout << "Time elapsed since scheduler-call: " << tElapsed.count() << std::endl;
+}
 
-        // java 8
-        scheduler_command(() -> System.out.println("Hello World"), 1000);
-    }
+auto anotherFunction() -> void { std::cout << "Another thread printed me!" << std::endl; }
 
-    public static void scheduler_command(Command command, int n) {
-        new Timer().schedule(new TimerTask() {
-            @Override
-            public void run() {
-                command.execute();
-            }
-        }, n);
+template <typename f>
+auto schedule(int sleeptime, f callback) -> void {
+    std::thread([sleeptime, callback]() {
+        std::chrono::milliseconds sleepDuration(sleeptime);
+        std::this_thread::sleep_for(sleepDuration);
+        callback();
+    })
+        .detach();
+}
+template <>
+auto schedule(int sleeptime,
+              void (*printElapsedTime)(std::chrono::time_point<std::chrono::high_resolution_clock>))
+    -> void {
+    std::thread([sleeptime, printElapsedTime]() {
+        std::chrono::milliseconds sleepDuration(sleeptime);
+        auto startTime = std::chrono::high_resolution_clock::now();
+        std::this_thread::sleep_for(sleepDuration);
+        printElapsedTime(startTime);
+    })
+        .detach();
+}
 
-    }
+int main() {
+    // note that the scheduled thread can outlive the main-tread
+    // main thread should live longer then the schedduled thread
+    schedule(500, printElapsedTime);
+    schedule(300, anotherFunction);
 
-    interface Command {
-        public void execute();
-    }
+    // helper function to keep the main tread alive for a bit
+    std::chrono::milliseconds sleepDuration(2000);
+    std::this_thread::sleep_for(sleepDuration);
+    return 0;
 }
